1160203b6d8b776e266835edac5082d29118cb32
ashley kildunne <ashleykildunne@ashleys-MBP.attlocal.net>
2020-09-23 13:36:10 -0700

    Components Connected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import Home from './screens/Home'
+// import Products from './screens/Products'
+// import ProductCreate from './screens/ProductCreate'
+// import ProductEdit from './screens/ProductEdit'
+// import ProductDetail from './screens/ProductDetail'
+import { Route, Switch } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-      
+       <Switch>
+        <Route exact path="/" component={Home} />
+        {/* <Route exact path="/products" component={Products} />
+        <Route exact path="/add-product" component={ProductCreate} />
+        <Route exact path="/products/:id/edit" component={ProductEdit} />
+        <Route exact path="/products/:id" component={ProductDetail} /> */}
+      </Switch>
     </div>
   );
 }
