de85a69f544e157f1b848a76ac98043512c0668d
ahsikuddin1 <67656602+ahsikuddin1@users.noreply.github.com>
2020-09-24 10:26:23 -0400

    Updates to schema -Ashik

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ const Product = new Schema(
     imgURL: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: String, required: true },
-    review: { type: String, required: true },
+
   },
   { timestamps: true }
 )
