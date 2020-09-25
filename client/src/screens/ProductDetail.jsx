1245bdea0f650855def9d7871044d200627284b9
ashley kildunne <ashleykildunne@ashleys-MBP.attlocal.net>
2020-09-25 08:27:56 -0700

    Product Detail Styling Start

--- client/src/screens/ProductDetail.jsx
+++ client/src/screens/ProductDetail.jsx
@@ -2,7 +2,24 @@ import React, { useState, useEffect } from 'react';
 // import Layout from '../components/shared/Layout';
 import { useParams } from 'react-router-dom';
 import { getProduct } from '../services/products';
-// import styled from "styled-components";
+import styled from "styled-components";
+
+const ProductDetailContainer = styled.div`
+  display: flex;  
+  flex-direction: column;
+  justify-content: center;
+  background-color: #40A48B;
+  width: auto;
+  height: 544px;
+  font: medium 22px/30px Futura;
+`
+
+const ImageContainer = styled.img`
+  height: 251px;
+  width: 251px;
+  margin-left: 123px;
+  border-radius: 15%;
+`
 
 const ProductDetail = (props) => {
   const [product, setProduct] = useState({
@@ -24,14 +41,14 @@ const ProductDetail = (props) => {
 
   return (
     <>
-      <div className="product-detail">
-        <img className="product-detail-image" src={product.imgURL} alt={product.product} />
+      <ProductDetailContainer>
+        <ImageContainer src={product.imgURL} alt={product.product} />
         <div className="detail">
           <div className="product">{product.product}</div>
           <div className="price">{`${product.price}`}</div>
           <div className="description">{product.description}</div>
         </div>
-      </div>
+      </ProductDetailContainer>
     </>
   )
 }
