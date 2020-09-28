e542ed85c2630c9e67ae1e2e7aa97a88fa3c7e7a
ahsikuddin1 <67656602+ahsikuddin1@users.noreply.github.com>
2020-09-28 11:34:37 -0400

    A-Z working for sort

--- client/src/utils/sort.js
+++ client/src/utils/sort.js
@@ -8,7 +8,7 @@ const compareKey = key =>
     }
     return 0
   }
-export const AZ = arr => arr.sort(compareKey('name'))
-export const ZA = arr => arr.sort(compareKey('name')).reverse()
+export const AZ = arr => arr.sort(compareKey('product'))
+export const ZA = arr => arr.sort(compareKey('product')).reverse()
 export const lowestFirst = arr => arr.sort((a, b) => parseInt(a.price) - parseInt(b.price))
 export const highestFirst = arr => arr.sort((a, b) => parseInt(b.price) - parseInt(a.price))
